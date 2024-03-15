785f1e0f5891b2272d880ac0afb5b5a6259db0e1
KostyaKavka <kostyakavka@icloud.com>
2024-03-15 10:59:40 +0300

    clean dev branch

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,18 +1,16 @@
-// const express = require('express');
 const router = require('express').Router();
-    
-    // express.Router();
 
 //view
 const authViewRouter = require('./views/auth.view.router');
 const mainRouter = require('./views/main.router')
-
+const questionPage=require('./views/questionpage.view.routes')
 //api
 const authApiRouter = require('./api/auth.api.router');
 
 //router view
 router.use('/auth', authViewRouter);
 router.use('/', mainRouter)
+router.use('/question', questionPage)
 
 //router api
 router.use('/api/auth', authApiRouter);
@@ -20,5 +18,3 @@ router.use('/api/auth', authApiRouter);
 
 
 module.exports = router;
-
-
