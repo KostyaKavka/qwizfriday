b97cc2ab5704cc258649f65f727c33585c0db8dc
Alexandr Zalucskii <san4ez1222@gmail.com>
2024-03-15 14:01:26 +0300

    FOTO

--- db/seeders/20240314150305-Category.js
+++ db/seeders/20240314150305-Category.js
@@ -7,14 +7,14 @@ module.exports = {
       "Categories",
       [
         {
-          title: "Фильмы",
-          img: "https://st3.depositphotos.com/1064045/15061/i/450/depositphotos_150614902-stock-photo-unusual-cinema-concept-3d-illustration.jpg",
+          title: "Кто? Что? Сколько? И может быть зачем?",
+          img: "https://lh3.googleusercontent.com/proxy/vG5WNAnsvWs05CDMA7TjrjjhNlOYGaWhJtvzyaro3Mmw2O-UDIhrsapq72DQ24zqn3cOmfEtX-X7sIoH7akxnLkAFVZveA",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
 
         {
-          title: "Театр",
+          title: "Сплетни Эльбруса",
           img: "https://media.istockphoto.com/id/1295114854/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BF%D1%83%D1%81%D1%82%D1%8B%D0%B5-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D1%8B%D0%B5-%D0%BA%D1%80%D0%B5%D1%81%D0%BB%D0%B0-%D1%82%D0%B5%D0%B0%D1%82%D1%80%D0%B0-%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D1%8B-%D0%BA-%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D1%83.jpg?s=612x612&w=0&k=20&c=BI-hxXNOUEBXHD00XDxGGv6auN8N9RQylsdG1-1h1Kc=",
           createdAt: new Date(),
           updatedAt: new Date(),
--- db/seeders/20240314150552-Question.js
+++ db/seeders/20240314150552-Question.js
@@ -7,18 +7,98 @@ module.exports = {
       "Questions",
       [
         {
-          answer: "5",
-          question: "сколько пальцев на руке?",
-          img: "https://img.freepik.com/free-photo/forest-landscape_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1710288000&semt=ais",
+          answer: "Ваня",
+          question: "Кто это у нас такой в тапках?",
+          img: "",
           categoryId: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
 
         {
+          answer: "Алины",
+          question: "Чьё это фотогениченое ухо?",
+          img: "",
+          categoryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+
+        {
+          answer: "В левом",
+          question: "Отгадайте в каком лифте этот врасавчик сделал фото?",
+          img: "",
+          categoryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+
+        {
+          answer: "В левом",
+          question: "В каком холодильнике столько пакетов?",
+          img: "",
+          categoryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+
+        {
+          answer: "Камил",
+          question: "Чья шикарная шевелюра на фото?",
+          img: "",
+          categoryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+
+        {
+          answer: "Правда",
+          question: "Правда что один из женихов является сыном Лианы",
+          img: "",
+          categoryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        
+        {
+          answer: "Правда",
+          question: "Правда что Даня (СасэоБамбэо) является крашем Димы и мы это не осуждаем?  ",
+          img: "",
+          categoryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        
+        {
+          answer: "Факт",
+          question: "Правда что Маша со 2 фазы осталась на повтор только ради Саши?",
+          img: "",
+          categoryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        
+        {
           answer: "5",
-          question: "сколько пальцев на руке?",
-          img: "https://i.pinimg.com/236x/24/15/21/24152197af38deb718eb730992d441d0.jpg",
+          question: "Правда что приход случается под конец второй фазы?",
+          img: "",
+          categoryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        
+        {
+          answer: "Пошуршим",
+          question: "Правда что мы секта?",
+          img: "",
           categoryId: 2,
           createdAt: new Date(),
           updatedAt: new Date(),
